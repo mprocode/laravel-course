3aeecef8e386bc615ac85d7561535f5fe9a0f77f
MPro Coding <mproconsultoria@gmail.com>
2020-05-25 05:38:50 -0400

    03-views - The @if directive

--- 03-views/resources/views/projects.blade.php
+++ 03-views/resources/views/projects.blade.php
@@ -18,13 +18,43 @@
     border-color: darkgrey;
     border-radius: 10px;
     background-color: #f5f5f5;
+
+    display: flex; 
+    justify-content: space-between; 
+    flex-direction: column;     
 }
 .card-container {
     display: flex; 
     justify-content: flex-start; 
     flex-direction: row; 
     flex-wrap:wrap;
-}    
+}
+.inactive {
+    height: 15px;
+    margin: 10px;
+    text-align: center;
+    background-color: indianred;
+} 
+.active {
+    height: 20px;
+    margin: 10px;
+    text-align: center;
+    background-color: mediumspringgreen;
+} 
+.loop-properties {
+    display: flex; 
+    justify-content: flex-start; 
+    flex-direction: row;
+    align-items: center;
+}
+.tag {
+    color:#fff!important;
+    background-color:#2196F3!important;
+    padding-left:8px;
+    padding-right:8px;
+    margin: 8px;
+    text-align:center
+}
 </style>
 
 <div class="card-container">
@@ -46,6 +76,28 @@
                 <span>#{{ $tag }}</span>
             @endforeach
         </div>
+
+        @if ($p->active)
+            <div class="active"> ACTIVE PROJECT </div>
+        @endif
+
+        @if (!$p->active)
+            <div class="inactive"> INACTIVE PROJECT </div>
+        @endif
+
+        <div class="loop-properties">
+            Loop Properties:
+            <span class="tag">
+                {{ $loop->index }} {{ $loop->iteration }}/{{ $loop->count }}
+            </span>
+
+            @if ($loop->first)  <span class="tag">First</span> @endif
+            @if ($loop->last)  <span class="tag">Last</span> @endif
+            @if ($loop->even)  <span class="tag">Even</span> @endif
+            @if ($loop->odd)  <span class="tag">Odd</span> @endif
+
+        </div>
+
         </div>
     @endforeach
 </div>
