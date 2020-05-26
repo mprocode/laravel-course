f33e863f99cd9d52e423f2c1bb1240a8e52ea12a
MPro Coding <mproconsultoria@gmail.com>
2020-05-26 03:49:42 -0400

    03-views - Moving styles to a file and using Laravel Mix to compile the assets

--- 03-views/resources/views/layouts/main.blade.php
+++ 03-views/resources/views/layouts/main.blade.php
@@ -8,51 +8,8 @@
     @yield('title')
   </title>
 
-  <style>
-    .row {
-        display: flex;
-    }
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    .col1 {
-        flex: 20%;
-        padding: 10px;
-    }
-
-    .col2 {
-        flex: 70%;
-        padding: 10px;
-    }
-
-    .menu ul {
-        margin: 0px;
-        padding: 0px;
-        list-style-type: none;
-    }
-
-    .menu li { list-style: none; }
-
-    .menu a {
-        display: block;
-        width: 200px;
-        height: 20px;
-        color: black;
-        background-color: #FFFFFF;
-        text-decoration: none;
-        text-align: center;
-        margin: 5px;
-        font-size: 18px;
-    }
-
-    .menu a:hover {
-        background-color: #b9bcd0;
-        color: #0000ff;
-        font-weight: bold;
-    }
-
-    .menu .active {
-        background-color: #c0d4f3;
-    }    
-  </style>
 </head>
 <body>
     <div class="row">
