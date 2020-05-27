c5c8c5de11327ec90b4d605273ad34be220f0d63
MPro Coding <mproconsultoria@gmail.com>
2020-05-27 06:02:46 -0400

    03-views - Creating the chart card component

---
+++ 03-views/resources/views/components/chart-card.blade.php
@@ -0,0 +1,3 @@
+<div class="border border-gray-400 rounded shadow-xl px-6 py-4 sm:w-auto md:w-auto lg:w-auto xl:w-1/4 m-4">
+    {{ $slot }}
+</div>
--- 03-views/resources/views/home.blade.php
+++ 03-views/resources/views/home.blade.php
@@ -5,61 +5,25 @@
 @section('content')
 
 
-<h1>Home</h1>
-
-<hr>
-
-
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js" integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" crossorigin="anonymous"></script>
+    <script 
+        src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js" 
+        integrity="sha256-XF29CBwU1MWLaGEnsELogU6Y6rcc5nCkhhx89nFMIDQ=" 
+        crossorigin="anonymous">
+    </script>
 @endpush
 
-@push('styles')
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-<style>
-.shadow {
-    box-shadow: 0 1px 1px rgba(0,0,0,0.12), 
-              0 2px 2px rgba(0,0,0,0.12), 
-              0 4px 4px rgba(0,0,0,0.12), 
-              0 8px 8px rgba(0,0,0,0.12),
-              0 16px 16px rgba(0,0,0,0.12);
-}
-.card { 
-    width: 512px; 
-    margin: 20px;
-    padding: 10px;
-    border: 1px;
-    border-style: solid;
-    border-color: darkgrey;
-    border-radius: 10px;
-    background-color: #f5f5f5;
-    display: flex; 
-    justify-content: space-between; 
-    flex-direction: column;     
-}
-.card-container {
-    display: flex; 
-    justify-content: flex-start; 
-    flex-direction: row; 
-    flex-wrap:wrap;
-}
-</style>
-@endpush
-
-<div class="card-container">
+<div class="flex flex-row flex-wrap items-stretch">
     @foreach ($charts as $chart)
-        <div class="card shadow">
-            <div>
-                <canvas id="chartjs-{{ $loop->index }}" class="chartjs" ></canvas>
-                <script>
-                    new Chart(
-                        document.getElementById("chartjs-{{ $loop->index }}"),
-                        @json($chart)
-                    );
-                </script>
-            </div>
-        </div>
+        <x-chart-card>
+            <canvas id="chartjs-{{ $loop->index }}" class="chartjs" ></canvas>
+            <script>
+                new Chart(
+                    document.getElementById("chartjs-{{ $loop->index }}"),
+                    @json($chart)
+                );
+            </script>
+        </x-chart-card>
     @endforeach
 </div>
 
